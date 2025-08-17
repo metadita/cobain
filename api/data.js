81399bca2915638d1911b5b179320193f264e637
metadita <tigano.er@gmail.com>
2025-08-17 23:17:21 +0700

    Update data.js

--- api/data.js
+++ api/data.js
@@ -1,4 +1,4 @@
-import { Redis } from '@upstash/redis';
+import { Redis } from "@upstash/redis";
 
 const redis = new Redis({
   url: process.env.KV_REST_API_URL,
@@ -7,16 +7,14 @@ const redis = new Redis({
 
 export default async function handler(req, res) {
   try {
-    // Ambil nilai awal
-    let nilai = await redis.get('nilai_awal');
+    let nilai = await redis.get("nilai_awal");
     if (nilai === null) {
       nilai = 10000; // default kalau belum ada
-      await redis.set('nilai_awal', nilai);
+      await redis.set("nilai_awal", nilai);
     }
-
     res.status(200).json({ nilai_awal: nilai });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Gagal memuat nilai' });
+    res.status(500).json({ error: "Gagal memuat nilai" });
   }
-  }
+}
