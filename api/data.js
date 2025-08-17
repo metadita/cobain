686c963f4227d3aefc4efb82eadfd96f6b54695f
metadita <tigano.er@gmail.com>
2025-08-17 22:37:16 +0700

    Update data.js

--- api/data.js
+++ api/data.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), 'api', 'nilais.json');
+  const filePath = path.join(process.cwd(), 'data', 'nilais.json');
   const data = fs.readFileSync(filePath, 'utf-8');
   res.setHeader('Content-Type', 'application/json');
   res.status(200).send(data);
